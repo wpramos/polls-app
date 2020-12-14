d768df085b1ab4d9d175c9c252f83de731463081
Amos P <amosp323@gmail.com>
2020-12-14 16:06:57 +0530

    Add detail, results and vote views
    - Wire views to polls URLConf

--- src/polls/views.py
+++ src/polls/views.py
@@ -6,3 +6,16 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, World. You're at the polls index.")
+
+
+def detail(request, question_id):
+    response = "You're at the details page of question {}."
+    return HttpResponse(response.format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}."
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    response = "You're voting on question {}."
+    return HttpResponse(response.format(question_id))
