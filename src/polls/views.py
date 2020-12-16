18fc08aa001d042b938ff9a4d651142291348508
Amos P <amosp323@gmail.com>
2020-12-16 16:07:30 +0530

    Add index.html template to polls app
    - Update index view with template

--- src/polls/views.py
+++ src/polls/views.py
@@ -2,10 +2,16 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+from .models import Question
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        "latest_question_list": latest_question_list, 
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
