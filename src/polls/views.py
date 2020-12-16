2c74bebbb83b8f11ed22febd0107d039b02ec27a
Amos P <amosp323@gmail.com>
2020-12-16 16:29:51 +0530

    Update detail view to raise Http404 exception
    Add detail.html template to polls app

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -9,14 +9,23 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
-        "latest_question_list": latest_question_list, 
+        'latest_question_list': latest_question_list, 
     }
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    response = "You're at the details page of question {}."
-    return HttpResponse(response.format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist.')
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
+    # response = "You're at the details page of question {}."
+    # return HttpResponse(response.format(question_id))
 
 def results(request, question_id):
     response = "You're looking at the results of question {}."
