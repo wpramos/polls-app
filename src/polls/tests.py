a9d93468e5525ee68ef475c155f12f9ca51eb296
Amos P <amosp323@gmail.com>
2020-12-23 16:30:54 +0530

    Add first test for Question model
    - To test if method was_published_recently() returns False in the case of future question

--- src/polls/tests.py
+++ src/polls/tests.py
@@ -1,3 +1,17 @@
+import datetime
+
 from django.test import TestCase
 
+from django.utils import timezone
+
+from .models import Choice, Question
+
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+    
+    def test_was_published_recently_with_future_question(self):
+        future_time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_time)
+
+        self.assertIs(future_question.was_published_recently(), False)
