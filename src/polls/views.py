59f9cc8c3e1ef315828b3a9a9c94858acb3a1685
Amos P <amosp323@gmail.com>
2021-01-01 18:39:56 +0530

    Add get_queryset() method to DetailView and ResultsView classes
    - To filter the queryset to contain only published questions, so that no unpublished question [i.e. with pub_date in the future] is displayed

--- src/polls/views.py
+++ src/polls/views.py
@@ -39,7 +39,10 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
 #     context = {
@@ -50,6 +53,9 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
