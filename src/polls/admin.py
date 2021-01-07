fef4673ba2efe980bdf73f8c639df49c86d080e3
Amos P <amosp323@gmail.com>
2021-01-07 15:14:18 +0530

    Create and register QuestionAdmin model admin class
    - To customize the admin options of the model

--- src/polls/admin.py
+++ src/polls/admin.py
@@ -4,4 +4,10 @@ from .models import Question
 
 # Register your models here.
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information:', {'fields': ['pub_date']})
+        ]
+
+admin.site.register(Question, QuestionAdmin)
