f362f5e3ad45b5d7aeff751a26a8f7540c6e8663
Amos P <amosp323@gmail.com>
2021-01-07 16:05:36 +0530

    Create ChoiceInline inline model admin
    - Add ChoiceInlines to QuestionsAdmin using the class' 'inlines' attribute

--- src/polls/admin.py
+++ src/polls/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information:', {'fields': ['pub_date']})
+        ('Date information:', {
+                               'fields': ['pub_date'], 
+                               'classes': ['collapse']
+                               })
         ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
